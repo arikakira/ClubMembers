f6dade3b79dea898368fede0b65ad42557cce2ef
arikakira <samanthal242@nycstudents.net>
2025-04-11 20:52:56 +0000

    Update

--- ClubMembers.java
+++ ClubMembers.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
 
+public class ClubMembers {
+    private ArrayList<MemberInfo> memberList;
+
+
+    /**
+     * Adds new club members to memberList, as described in part (a).
+     * Precondition: names is a non-empty array.
+     */
+    public void addMembers(String[] names, int gradYear) {
+        /* to be implemented in part (a) */ }
+
+
+    /**
+     * Removes members who have graduated and returns a list of members who have
+     * graduated
+     * and are in good standing, as described in part (b).
+     */
+    public ArrayList<MemberInfo> removeMembers(int year) {
+        /* to be implemented in part (b) */ }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
